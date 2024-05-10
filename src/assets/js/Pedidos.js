d70247dfce51f6f830f7cb4f4888da550be67f0f
gabrielroeder <gabrielroeder@gmail.com>
2024-05-09 22:16:47 -0300

    fix: ajustes na página de pedidos e seu javascript

--- src/assets/js/Pedidos.js
+++ src/assets/js/Pedidos.js
@@ -1,121 +1,41 @@
-//CARD 1//
-const avaliarBtn = document.getElementById('avaliarBtn');
-const avaliacaoContainer = document.getElementById('avaliacaoContainer');
-const estrelas = document.querySelectorAll('.estrela');
+const MINUTE = 60000
+const minutesSpan = document.querySelectorAll('.minutos')
 
-avaliarBtn.addEventListener('click', () => {
-  avaliacaoContainer.classList.remove('hidden');
-});
-
-estrelas.forEach(estrela => {
-  estrela.addEventListener('click', () => {
-    const valor = estrela.getAttribute('data-valor');
-    alert('Você avaliou seu pedido como ' + valor + ' estrelas. Obrigado!');
-    estrela.classList.add('selecionada');   
-    
-    localStorage.setItem('avaliacaoPedido', valor);   
-    
-    avaliarBtn.textContent = 'Pedido Avaliado';
-    estrelas.forEach(estrela => estrela.removeEventListener('click'));    
-    estrelas.forEach(estrela => {
-      
-      
-    });
-  });
-});
-
-//CARD 2//
-document.addEventListener("DOMContentLoaded", function() {
-  const startButton = document.getElementById("startButton");
-  const timerElement = document.getElementById("timer");
-   
-  let timer;
-  let timeLeft = 12 * 60; // 15 minutos em segundos
-
-  startButton.addEventListener("click", function() {
-    if (!timer) {
-      timer = setInterval(updateTimer, 1000);
-      updateTimer();
-    }
-  });
-
-  function updateTimer() {
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      timer = null;
-      alert("Gentileza buscar seu pedido");
-      timerElement.textContent = "";      
-      startButton.textContent = "Pedido Pronto";
+minutesSpan.forEach(element => {
+  let minutesLeft = element.textContent;
+  setInterval(function(){
+    if(minutesLeft > 0) {
+      minutesLeft = minutesLeft - 1;
+      element.textContent = minutesLeft
     } else {
-      const minutes = Math.floor(timeLeft / 60);
-      const seconds = timeLeft % 60;
-      timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-      timeLeft--;
+      element.parentElement.parentElement.textContent = 'PEDIDO ATRASADO!'
+    }
+  }, MINUTE)
+});
+
+const STARQUANTITY = 5
+
+const ratingSection = document.querySelector('.rating');
+for (let index = 5; index > 0; index--) {
+  ratingSection.insertAdjacentHTML('afterbegin',
+    `<i class="fa-regular fa-star" data-star-id=${index} style="cursor: pointer;" onclick="starClicked(this)"></i>`
+  )
+}
+
+function starClicked(element) {
+  const starPosition = element.getAttribute('data-star-id')
+  document.querySelectorAll('.fa-star').forEach(elementStar => {
+    elementStar.remove()
+  });
+  for(let index = 5; index > 0; index--) {
+    if(index <= starPosition) {
+      ratingSection.insertAdjacentHTML('afterbegin',
+        `<i class="fa-solid fa-star" data-star-id=${index}"></i>`
+      )
+    } else {
+      ratingSection.insertAdjacentHTML('afterbegin',
+        `<i class="fa-regular fa-star" data-star-id=${index}"></i>`
+      )
     }
   }
-});
-
-//CARD 3//
-  document.addEventListener("DOMContentLoaded", function() {
-    const iniciarBotao = document.getElementById("iniciarBotao");
-    const elementoTemporizador = document.getElementById("temporizador");
-     
-    let temporizador;
-    let tempoRestante = 22 * 60; // 15 minutos em segundos
-  
-    iniciarBotao.addEventListener("click", function() {
-      if (!temporizador) {
-        temporizador = setInterval(atualizarTemporizador, 1000);
-        atualizarTemporizador();
-      }
-    });
-  
-    function atualizarTemporizador() {
-      if (tempoRestante <= 0) {
-        clearInterval(temporizador);
-        temporizador = null;
-        alert("Gentileza buscar seu pedido");
-        elementoTemporizador.textContent = "";        
-        iniciarBotao.textContent = "Pedido Pronto";
-      } else {
-        const minutos = Math.floor(tempoRestante / 60);
-        const segundos = tempoRestante % 60;
-        elementoTemporizador.textContent = `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
-        tempoRestante--;
-      }
-    }
-});
-
-//CARD 4//
-document.addEventListener("DOMContentLoaded", function() {
-  const botaoInicio = document.getElementById("botaoInicio");
-  const elementoTemporizador = document.getElementById("elementoTemporizador");
-  const avaliarBtn = document.getElementById('avaliarBtn');
-  const avaliacaoContainer = document.getElementById('avaliacaoContainer');
-  const estrelas = document.querySelectorAll('.estrela');
-
-  let temporizador;
-  let tempoRestante = 17 * 60; // 15 minutos em segundos
-
-  botaoInicio.addEventListener("click", function() {
-      if (!temporizador) {
-          temporizador = setInterval(atualizarTemporizador, 1000);
-          atualizarTemporizador();
-      }
-  });
-
-  function atualizarTemporizador() {
-      if (tempoRestante <= 0) {
-          clearInterval(temporizador);
-          temporizador = null;
-          alert("Gentileza buscar seu pedido");
-          elementoTemporizador.textContent = "";          
-          botaoInicio.textContent = "Pedido Pronto";
-          } else {
-          const minutos = Math.floor(tempoRestante / 60);
-          const segundos = tempoRestante % 60;
-          elementoTemporizador.textContent = `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
-          tempoRestante--;
-      }
-    }
-});
+}
