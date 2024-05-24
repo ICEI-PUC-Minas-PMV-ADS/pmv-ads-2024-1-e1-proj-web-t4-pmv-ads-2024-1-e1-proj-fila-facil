337fd9df09a2817065a575073f94690a9580f40d
GabrielGBrasil <135841856+GabrielGBrasil@users.noreply.github.com>
2024-05-23 21:17:18 -0300

    login.js

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,54 +1,27 @@
-const authenticatedUser = JSON.parse(sessionStorage.getItem('authenticatedUser') || '[]');
-if (authenticatedUser.length !== 0) {
-    redirectToIndex();
-}
-
 const formSection = document.querySelector('.mensagem');
 const loginButton = document.getElementById('BtnEntrar');
 const emailInput = document.getElementById('email_user');
 const passwordInput = document.getElementById('senha_user');
 
-function createAlertMessage(alertType, message) {
-    return `
-        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-            <strong>Erro!</strong> ${message}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>`;
+
+function getUserFromLocalStorage(email) {
+  return JSON.parse(localStorage.getItem(email));
 }
 
 
-function redirectToIndex() {
-    window.location.href = '../index.html';
-}
+loginButton.addEventListener('click', function (event) {
+  event.preventDefault(); 
 
 
-function redirectToRestaurantes() {
-    window.location.href = 'restaurantes.html';
-}
+  var user = getUserFromLocalStorage(emailInput.value);
 
-loginButton.addEventListener('click', function(event) {
-    event.preventDefault();
+  if (user && user.password === passwordInput.value) {
+    alert("Login realizado com sucesso!");
 
-    const existingAlert = formSection.querySelector('.alert');
-    if (existingAlert) {
-        formSection.removeChild(existingAlert);
-    }
-
-    const email = emailInput.value.trim();
-    const password = passwordInput.value;
-
-    if (!email || !password) {
-        const alertMessage = createAlertMessage('warning', 'Os campos de email e senha não podem ser vazios.');
-        formSection.insertAdjacentHTML('afterbegin', alertMessage);
-        return;
-    }
-
-    if (email === 'admin' && password === 'adm123') {
-        const userInfo = { email: 'admin', password: 'adm123' };
-        sessionStorage.setItem('authenticatedUser', JSON.stringify(userInfo));
-        redirectToRestaurantes();
-    } else {
-        const alertMessage = createAlertMessage('danger', 'Usuário ou senha inválido. Tente novamente.');
-        formSection.insertAdjacentHTML('afterbegin', alertMessage);
-    }
+    window.location.href = "restaurantes.html";
+  } else {
+    formSection.textContent = "E-mail ou senha incorretos!";
+    formSection.style.display = "block";
+    formSection.style.color = "red"; 
+  }
 });
