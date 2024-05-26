74a4274a9fea8da20988cf8fb7f02a3332ed701b
GabrielGBrasil <135841856+GabrielGBrasil@users.noreply.github.com>
2024-05-26 18:35:19 -0300

    login.js

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -12,16 +12,36 @@ function getUserFromLocalStorage(email) {
 loginButton.addEventListener('click', function (event) {
   event.preventDefault(); 
 
+  if (emailInput.value != "" && passwordInput.value != "") {
+    
+    var user = getUserFromLocalStorage(emailInput.value);
 
-  var user = getUserFromLocalStorage(emailInput.value);
+    if (user && user.password === passwordInput.value) {
+      alert("Login realizado com sucesso!");
+      window.location.href = "restaurantes.html";
+    } else if (user) {
+      formSection.textContent = "Senha incorreta!";
+      formSection.style.display = "block";
+      formSection.style.color = "red";
+    } else {
+      formSection.textContent = "Usuário não encontrado!";
+      formSection.style.display = "block";
+      formSection.style.color = "red";
+    }
 
-  if (user && user.password === passwordInput.value) {
-    alert("Login realizado com sucesso!");
+  }
 
-    window.location.href = "restaurantes.html";
-  } else {
-    formSection.textContent = "E-mail ou senha incorretos!";
+  if (emailInput.value === "") {
+    formSection.textContent = "O campo de email não pode estar vazio!";
     formSection.style.display = "block";
-    formSection.style.color = "red"; 
+    formSection.style.color = "red";
+    return;
+  }
+
+  if (passwordInput.value === "") {
+    formSection.textContent = "O campo de senha não pode estar vazio!";
+    formSection.style.display = "block";
+    formSection.style.color = "red";
+    return;
   }
 });
