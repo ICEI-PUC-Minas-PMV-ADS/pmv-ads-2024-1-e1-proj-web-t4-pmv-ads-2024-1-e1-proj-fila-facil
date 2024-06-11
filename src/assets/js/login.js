67479b1f0de3b1dcf4176ae7de27e4ee0d38fe9b
Ytallo <ytallobruno@hotmail.com>
2024-06-11 20:55:34 -0300

    feat: Criada validação para usuário logado
    Ajustes na navbar da página de pedidos e em demais lógicas envolvidas no cadastro/login de usuario

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,24 +1,22 @@
-const formSection = document.querySelector('.mensagem');
-const loginButton = document.getElementById('BtnEntrar');
-const emailInput = document.getElementById('email_user');
-const passwordInput = document.getElementById('senha_user');
-
+const formSection = document.querySelector(".mensagem");
+const loginButton = document.getElementById("BtnEntrar");
+const emailInput = document.getElementById("email_user");
+const passwordInput = document.getElementById("senha_user");
 
 function getUserFromLocalStorage(email) {
   return JSON.parse(localStorage.getItem(email));
 }
 
-
-loginButton.addEventListener('click', function (event) {
-  event.preventDefault(); 
+loginButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
   if (emailInput.value != "" && passwordInput.value != "") {
-    
     var user = getUserFromLocalStorage(emailInput.value);
 
     if (user && user.password === passwordInput.value) {
       alert("Login realizado com sucesso!");
       window.location.href = "restaurantes.html";
+      localStorage.setItem("userLoggedIn", "true");
     } else if (user) {
       formSection.textContent = "Senha incorreta!";
       formSection.style.display = "block";
@@ -28,7 +26,6 @@ loginButton.addEventListener('click', function (event) {
       formSection.style.display = "block";
       formSection.style.color = "red";
     }
-
   }
 
   if (emailInput.value === "") {
