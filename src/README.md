576919a7884a4415c93d40f3b5b3f43c811004df
Diego Chaves dos Anjos <164424865+diiegochaves@users.noreply.github.com>
2024-06-23 10:08:45 -0300

    feat: Botão de pesquisa na página de restaurantes (#107)
    Co-authored-by: gabrielroeder <gabrielroeder@gmail.com>
    Co-authored-by: Gabriel Roeder <gabriel.roeder@redeunifique.com.br>

--- src/README.md
+++ src/README.md
@@ -291,6 +291,13 @@ O site em HTML/CSS/JS é um projeto estático. É necessário implantá-lo em um
 
 - Adicionado links das imagens nos pratos do cardápio
 
-### [0.1.34] - DD/MM/AAAA
+### [0.1.34] - 21/06/2024
+
+#### Adicionado
+
+- Adicionado botão funcional de pesquisa página restaurantes.
+- Fotos dos restaurantes hospedados externamente
+
+### [0.1.35] - DD/MM/AAAA
 
 #### Adicionado/Atualizado/Removido
--- src/assets/js/restaurantes.js
+++ src/assets/js/restaurantes.js
@@ -9,20 +9,32 @@ document.addEventListener("DOMContentLoaded", async function() {
     const container = document.getElementById('card-container');
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
-    let currentPage = 1;
-    const cardsPerPage = 3;
-    let restaurantData = restaurantes; // Use the imported data
+    const searchInput = document.getElementById('search-input');
+    let currentPage = 1; // Inicializa a página atual como 1
+    const cardsPerPage = 3; // Define o número de cards por página
+    // Obtém os dados dos restaurantes do localStorage, ou uma lista vazia se não houver
+    let restaurantData = JSON.parse(localStorage.getItem('restaurantes')) || [];
 
-    function renderRestaurants(page) {
+    // Função para renderizar os cards dos restaurantes
+    function renderRestaurants(page, data) {
+        // Limpa o conteúdo do container antes de adicionar novos cards
         container.innerHTML = '';
+        // Calcula os índices de início e fim dos dados a serem exibidos na página atual
         const start = (page - 1) * cardsPerPage;
         const end = start + cardsPerPage;
-        const restaurantsToDisplay = restaurantData.slice(start, end);
+        // Seleciona os dados dos restaurantes a serem exibidos na página atual
+        const restaurantsToDisplay = data.slice(start, end);
         
+        // Itera sobre os dados dos restaurantes selecionados e cria um card para cada um
         restaurantsToDisplay.forEach(restaurant => {
             const cardElement = document.createElement('div');
             cardElement.classList.add('card', 'card-div');
+            cardElement.addEventListener('click', () => {
+                // Redireciona para a página de cardápio ao clicar no card do restaurante
+                window.open(`cardapio.html?id=${restaurant.idRestaurante}`, '_blank');
+            });
 
+            // Adiciona um evento de clique à imagem para redirecionar para a página de cardápio
             const imgElement = document.createElement('img');
             imgElement.src = restaurant.imagemRestaurante;
             cardElement.appendChild(imgElement);
@@ -32,36 +44,55 @@ document.addEventListener("DOMContentLoaded", async function() {
 
             const titleElement = document.createElement('h2');
             titleElement.className = 'card-title';
-            titleElement.textContent = restaurant.nomeRestaurante; // Corrigido para acessar o nome do restaurante
+            titleElement.textContent = restaurant.nomeRestaurante;
             cardContent.appendChild(titleElement);
 
             const descriptionElement = document.createElement('p');
             descriptionElement.className = 'card-description';
-            descriptionElement.textContent = restaurant.descricaoRestaurante; 
+            descriptionElement.textContent = restaurant.descricaoRestaurante;
             cardContent.appendChild(descriptionElement);
 
             cardElement.appendChild(cardContent);
             container.appendChild(cardElement);
         });
 
+        // Desabilita o botão "Anterior" se estiver na primeira página
         prevBtn.disabled = page === 1;
-        nextBtn.disabled = end >= restaurantData.length;
+        // Desabilita o botão "Próximo" se estiver na última página
+        nextBtn.disabled = end >= data.length;
     }
 
-    renderRestaurants(currentPage); // Render 
+    // Função para filtrar os restaurantes com base no texto de busca
+    function filterRestaurants() {
+        const searchText = searchInput.value.toLowerCase();
+        // Filtra os dados dos restaurantes com base no texto de busca
+        const filteredData = restaurantData.filter(restaurant =>
+            restaurant.nomeRestaurante.toLowerCase().includes(searchText) ||
+            restaurant.descricaoRestaurante.toLowerCase().includes(searchText)
+        );
+        currentPage = 1; // Resetar para a primeira página ao buscar
+        // Renderiza os cards dos restaurantes filtrados
+        renderRestaurants(currentPage, filteredData);
+    }
 
+    // Adiciona um ouvinte de evento de input ao campo de busca para filtrar os restaurantes em tempo real
+    searchInput.addEventListener('input', filterRestaurants);
+
+    // Renderiza os cards dos restaurantes com os dados do localStorage ao carregar a página
+    renderRestaurants(currentPage, restaurantData);
+
+    // Adiciona ouvintes de evento aos botões de navegação entre páginas
     prevBtn.addEventListener('click', () => {
         if (currentPage > 1) {
             currentPage--;
-            renderRestaurants(currentPage);
+            renderRestaurants(currentPage, restaurantData);
         }
     });
 
     nextBtn.addEventListener('click', () => {
         if ((currentPage * cardsPerPage) < restaurantData.length) {
             currentPage++;
-            renderRestaurants(currentPage);
+            renderRestaurants(currentPage, restaurantData);
         }
     });
 });
- 
